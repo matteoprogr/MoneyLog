a8a836c5f8fc67a1bf45f0fe39e20d40a1d7d256
matteoprogr <matteo93parisi@gmail.com>
2025-09-16 15:28:08 +0200

    fix visualizzazione sezione info v18

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'moneylog-cache-v17';
+const CACHE_NAME = 'moneylog-cache-v18';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/styles.css?v=17',
-  '/main.js?v=17',
+  '/styles.css?v=18',
+  '/main.js?v=18',
   '/icons/icon-192.png',
   '/icons/icon-512.png'
 ];
